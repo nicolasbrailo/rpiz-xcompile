07d0679a39bf4946e104bb764492d25dcee2e726
Nico B <nicolasbrailo@gmail.com>
2024-10-27 19:35:20 +0000

    Make target directory configurable

--- mount_rpy_root.sh
+++ mount_rpy_root.sh
@@ -4,26 +4,34 @@ set -euo pipefail
 
 IMG_URL=https://downloads.raspberrypi.com/raspios_armhf/images/raspios_armhf-2024-07-04/2024-07-04-raspios-bookworm-armhf.img.xz
 
+# Get target dir from argv[1], use script dir if not present
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TGT_DIR=${1:-"$SCRIPT_DIR"}
+
 IMG_FNAME=$(basename "$IMG_URL" .xz)
+IMG_PATH="$TGT_DIR/$IMG_FNAME"
+MNT_PATH="$TGT_DIR/mnt"
 
 # If img doesn't exist, fetch it and uncompress it
-if [ ! -e "$IMG_FNAME" ]; then
-  if [ ! -e "$IMG_FNAME.xz" ]; then
+if [ ! -e "$IMG_PATH" ]; then
+  if [ ! -e "$IMG_PATH.xz" ]; then
     echo "Fetching image..."
-    wget "$IMG_URL"
+    wget --directory-prefix="$TGT_DIR" "$IMG_URL"
   fi
   echo "Uncompressing image..."
+
+  pushd "$TGT_DIR"
 	xz -d "$IMG_FNAME.xz"
+  popd
 fi
 
 # Mount image, if not mounted already
-IMG_FULLPATH=$(pwd)/"$IMG_FNAME"
-if [[ $( mount | grep -c "$IMG_FULLPATH" ) -eq 0 ]]; then
+if [[ $( mount | grep -c "$IMG_PATH" ) -eq 0 ]]; then
   # Find the offset for the root partition and mount
-  offset=$( sudo fdisk -lu "$IMG_FNAME" | grep Linux | awk '{print $2}' )
+  offset=$( sudo fdisk -lu "$IMG_PATH" | grep Linux | awk '{print $2}' )
   offset=$((512 * offset))
-  mkdir -p mnt
-  sudo mount -o loop,offset=$offset "$IMG_FNAME" ./mnt
+  mkdir -p "$MNT_PATH"
+  sudo mount -o loop,offset=$offset "$IMG_PATH" "$MNT_PATH"
 fi
 
 
--- umount_rpy_root.sh
+++ umount_rpy_root.sh
@@ -2,5 +2,10 @@
 
 set -euo pipefail
 
-sudo umount ./mnt
+# Get target dir from argv[1], use script dir if not present
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TGT_DIR=${1:-"$SCRIPT_DIR"}
+MNT_PATH="$TGT_DIR/mnt"
+
+sudo umount "$MNT_PATH"
 
