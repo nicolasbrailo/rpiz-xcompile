912694c1376413fe1b67b3248fb97c254940d6a6
Nico B <nicolasbrailo@gmail.com>
2024-11-16 20:17:59 +0000

    Support non-dev pkgs

--- add_sysroot_pkg.sh
+++ add_sysroot_pkg.sh
@@ -2,6 +2,17 @@
 
 set -euo pipefail
 
+USAGE="""
+You can use a running target to get the correct packages, and the uri to the packages. Eg:
+
+$ sudo apt list --installed | grep xkbcommon
+libxkbcommon-dev/stable,now 1.5.0-1 armhf [installed]
+libxkbcommon0/stable,now 1.5.0-1 armhf [installed]
+
+$ apt-get download --print-uris libxkbcommon-dev
+'http://raspbian.raspberrypi.com/raspbian/pool/main/libx/libxkbcommon/libxkbcommon-dev_1.5.0-1_armhf.deb' libxkbcommon-dev_1.5.0-1_armhf.deb 50996 SHA256:22f86a1c6c1b535d1c071885c41547cb4ec4beeddea8e9fc209a91ca80bc807b
+"""
+
 # Get target dir from argv[1], use script dir if not present
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SYSROOT_DIR=${1:-"$SCRIPT_DIR"}
@@ -30,8 +41,23 @@ if [ ! -d "$PKG_EXTRACT_DIR" ]; then
     dpkg-deb -R "$PKG_CACHE/$PKG_FNAME" "$PKG_EXTRACT_DIR"
 fi
 
-sudo cp -r "$PKG_EXTRACT_DIR/usr/include/"* "$SYSROOT_DIR/mnt/usr/include"
-sudo cp -r "$PKG_EXTRACT_DIR/usr/lib/"*     "$SYSROOT_DIR/mnt/usr/lib"
-sudo cp -r "$PKG_EXTRACT_DIR/usr/share/"*   "$SYSROOT_DIR/mnt/usr/share"
+DID_SOMETHING=0
+if [ -d "$PKG_EXTRACT_DIR/usr/include/" ]; then
+  sudo cp -r "$PKG_EXTRACT_DIR/usr/include/"* "$SYSROOT_DIR/mnt/usr/include"
+  DID_SOMETHING=1
+fi
+if [ -d "$PKG_EXTRACT_DIR/usr/lib/" ]; then
+  sudo cp -r "$PKG_EXTRACT_DIR/usr/lib/"*     "$SYSROOT_DIR/mnt/usr/lib"
+  DID_SOMETHING=1
+fi
+if [ -d "$PKG_EXTRACT_DIR/usr/share/" ]; then
+  sudo cp -r "$PKG_EXTRACT_DIR/usr/share/"*   "$SYSROOT_DIR/mnt/usr/share"
+  DID_SOMETHING=1
+fi
 
-echo "[re]installed $PKG_URL to sysroot"
+if [[ "$DID_SOMETHING" -eq 1 ]]; then
+  echo "[re]installed $PKG_URL to sysroot"
+else
+  echo "Failed to [re]install $PKG_URL to sysroot (nothing to copy?)"
+  exit 1
+fi
